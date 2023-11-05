69d4e6a1886a5f6e5d0a317f29d8e80f8aa3c6d9
Sheila <sheila.martinezdiez@gmail.com>
2023-11-05 17:20:14 +0100

    fixing loop

--- backend/index.js
+++ backend/index.js
@@ -15,31 +15,15 @@ const client = new Client({
 client.connect();
 
 app.get("/api", async (_request, response) => {
-  const motos = ["Harley", "Honda", "KAWA", "YAMAHA", "DUC"];
+  const { rows } = await client.query("SELECT * FROM mc WHERE name = $1", [
+    "Harley", "Honda", "KAWA", "YAMAHA", "DUC"
+  ]);
 
-  const placeholders = motos.map((_, index) => `$${index + 1}`).join(", ");
-
-  const query = `SELECT * FROM mc WHERE name IN (${placeholders})`;
-
-  try {
-    const { rows } = await client.query(query, motos);
-    response.json(rows);
-  } catch (error) {
-    console.error("Error al obtener datos:", error);
-    response.status(500).json({ error: "Error al obtener datos" });
-  }
+  response.send(rows);
 });
 
-// app.get("/api", async (_request, response) => {
-//   const { rows } = await client.query("SELECT * FROM mc WHERE name = $1", [
-//     "Harley", "Honda", "KAWA", "YAMAHA", "DUC"
-//   ]);
+app.use(express.static(path.join(path.resolve(), "public")));
 
-//   response.send(rows);
-// });
-
-// app.use(express.static(path.join(path.resolve(), "public")));
-
-// app.listen(3000, () => {
-//   console.log("Redo på http://localhost:3000/");
-// });
+app.listen(3000, () => {
+  console.log("Redo på http://localhost:3000/");
+});
