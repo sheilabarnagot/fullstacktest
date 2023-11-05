a3ae709f4bc1e3fbb5cb6d47d9943b65ce9b7d3c
Sheila <sheila.martinezdiez@gmail.com>
2023-11-05 16:25:43 +0100

    to DB

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,29 @@
-// Ska vi skapa kod eller hämta ?
+const express = require("express"),
+  path = require("path");
 
-const express = require('express'),
-  path = require('path')
+const app = express();
 
-const app = express()
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
 
-app.get('/api', (_request, response) => {
-  response.send({ hello: 'World' })
-})
+dotenv.config();
 
-app.use(express.static(path.join(path.resolve(), 'public')))
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+app.get("/api", async (_request, response) => {
+  const { rows } = await client.query("SELECT * FROM cities WHERE name = $1", [
+    "Stockholm",
+  ]);
+
+  response.send(rows);
+});
+
+app.use(express.static(path.join(path.resolve(), "public")));
 
 app.listen(3000, () => {
-  console.log('Redo på http://localhost:3000/')
-})
+  console.log("Redo på http://localhost:3000/");
+});
